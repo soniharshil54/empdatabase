25bfdf66385ebca2d4a5450128ac02e4f6e45376
harshil <sharshil43@yahoo.com>
2019-02-10 09:51:03 +0530

    first commit

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -25,6 +25,18 @@ router.post("/",function(req,res){
     newEmployee.save().then(employee => res.json(employee)).catch((err)=>res.json(err))
 })
 
+router.put("/:id",(req,res) => {
+    Employee.findById(req.params.id, (err, employee) => {
+            emp_name : req.body.emp_name,
+            emp_contact: req.body.emp_contact,
+            emp_designation:req.body.emp_designation,
+            emp_department:req.body.emp_department,
+            emp_gender:req.body.emp_gender
+            emloyee.save()
+            res.json(book)
+        }) 
+    })
+
 
 router.delete("/:id",function(req, res){
     Employee.findById(req.params.id).then(employee => employee.remove())
